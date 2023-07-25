fd11bd59d59565620f486c4d8533e5f8f458b4fc
Siddhant Dixit <89797613+Crazysniperr@users.noreply.github.com>
2023-07-25 17:53:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ function isLoggedIn(req, res, next) {
 app.use(express.json());
 app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Origin","https://crazysniperr-listy.netlify.app", "http://localhost:5173");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
